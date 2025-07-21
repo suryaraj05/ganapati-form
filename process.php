48339c828c16219b0ca1b5ac053100911f69edf5
Salve Surya Raj <ssuryabackup@gmail.com>
2025-07-21 19:29:18 +0530

    Added form with php

--- process.php
+++ process.php
@@ -1,19 +1,22 @@
 <?php
+    $to = "ssuryabackup@gmail.com";
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $city = isset($_POST['city']) ? $_POST['city'] : '';
+    $idol_type = isset($_POST['idol_type']) ? $_POST['idol_type'] : '';
+    $size = isset($_POST['size']) ? $_POST['size'] : '';
 
-    $to = "";
-    $from = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $name = $_REQUEST['name'];
-    $headers = "From: $from";
+    $subject = "New Ganesh Chaturthi 2025 Prebooking from $name";
+    $headers = "From: $email\r\nReply-To: $email\r\nContent-Type: text/plain; charset=UTF-8";
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"subject"} = "subject";
-    $fields{"message"} = "message";
-
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $body = "You have received a new Ganesh Chaturthi 2025 prebooking.\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "City: $city\n";
+    $body .= "Idol Type: $idol_type\n";
+    $body .= "Size: $size\n";
 
     $send = mail($to, $subject, $body, $headers);
-
 ?>
